c3a105d612cc7e1b75f85fb4a388bb02517899c6
Ida_Touray <tourayida428@gmail.com>
2026-07-05 15:22:13 -0700

    api update

---
+++ src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+// import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="Menu">
+            <ul>
+                <li className="Menu-item">
+                    <a className="menu-link" href="/">
+                        <i className="menu-icon tf-icons bx bx-home-circle"></i>
+                        <div>🏠 Dashboard</div>
+                    </a>
+                </li>
+
+                <li className="Menu-item">
+                    <a className="menu-link" href="/">
+                        <i className="menu-icon tf-icons bx bx-home-circle"></i>
+                        <div>🔍 Search</div>
+                    </a>
+                </li>
+
+                <li className="Menu-item">
+                    <a className="menu-link" href="/">
+                        <i className="menu-icon tf-icons bx bx-home-circle"></i>
+                        <div>ℹ️ About</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Menu;
